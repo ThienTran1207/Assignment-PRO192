41f3c7ea74393218d99a9734bcade8edc42fc5a2
ASUS <ASUS@LAPTOP-Q9LHHV8A>
2022-07-24 18:57:37 +0700

    sửa toString cho giống txt thầy cho

--- src/Brand.java
+++ src/Brand.java
@@ -1,4 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
 public class Brand {
+    
     String brandID;
     String brandName;
     String soundBrand;
@@ -48,8 +59,7 @@ public class Brand {
 
     @Override
     public String toString() {
-        return brandID + ", " + brandName + ", " + soundBrand + ", " + price;
+        return brandID + ", " + brandName + ", " + soundBrand +": " + price;
     }
-    
-    
+       
 }
