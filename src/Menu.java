a356800bfbd2f700d3aa4a9d23d830b4626bf523
ASUS <ASUS@LAPTOP-Q9LHHV8A>
2022-07-24 21:52:49 +0700

    Cap nhat Menu hoan chinh

--- src/Menu.java
+++ src/Menu.java
@@ -1,12 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
 
-public class Menu {
-    public static int getChoice (String[] ops){
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < ops.length; i++) {
-            System.out.println("\n" + (i+1) + "-" + ops[i]);           
+public class Menu<E> {
+    public int int_getChoice(ArrayList<E> options){
+        int response;
+        int N = options.size();
+        for (int i = 0; i <= N; i++){
+            if(options.get(i)!=null){
+                System.out.println((i+1) + ": " + options.get(i));
+            }
         }
-        System.out.println("\n Choose 1..." + ops.length + ": ");
-        return Integer.parseInt(sc.nextLine());
+        response = MyLibs.inputInt();
+        return response;
+    }
+    public E ref_getChoice(ArrayList<E> options){
+        int response;
+        int N = options.size();
+        do {            
+            response = int_getChoice(options);
+        } while (response < 0 || response > N);
+        return options.get(response - 1);
     }
 }
