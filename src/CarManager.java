dde4824fdff581286380ceb614956d43a0a9cada
NQMinh170486 <109325916+NQMinh170486@users.noreply.github.com>
2022-07-25 01:32:16 +0700

    Update thêm chức năng trong main

--- src/CarManager.java
+++ src/CarManager.java
@@ -1,15 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.ArrayList;
+import java.util.Scanner;
 
-/**
- *
- * @author ASUS
- */
 public class CarManager {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)  throws InterruptedException{
+        BrandList list = new BrandList();
+        Scanner sc = new Scanner(System.in);
+        String[] ops = {"List all brands", "Add a new brand", "Search a brand based on its ID",
+                        "Update a brand", "Save brands to the file, named brands.txt", 
+                        "List all cars in ascending order of brand names", "List cars based on a part of an input brand name",
+                        "Add a car", "Remove a car based on its ID", "Update a car based on its ID", "Save cars to file, named cars.txt"};
+        int choice = 0;
+        do{
+            choice = Menu.getChoice(ops);
+            switch (choice) {
+                case 1:
+                    list.listBrands();
+                    break;
+                case 2:
+                    list.addBrand();
+                    break;
+                case 3:
+                    System.out.print("Enter the ID: ");
+                    list.searchID(sc.nextLine());
+                    break;
+                case 4:
+                    list.updateBrand();
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    
+                    break;
+                case 8:
+                    
+                    break;
+                case 9:
+                    
+                    break;
+                case 10:
+                    
+                    break;
+                case 11:
+                    
+                    break;  
+            }
+        } while (choice >0  && choice <= 11);
     }
 }
