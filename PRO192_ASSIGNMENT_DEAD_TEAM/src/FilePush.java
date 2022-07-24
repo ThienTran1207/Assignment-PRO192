3b83080a8f1b712db5e9530d71fdb02cca9fd9cc
ASUS <ASUS@LAPTOP-Q9LHHV8A>
2022-07-24 15:00:51 +0700

    file chay push thu len git

--- PRO192_ASSIGNMENT_DEAD_TEAM/src/FilePush.java
+++ PRO192_ASSIGNMENT_DEAD_TEAM/src/FilePush.java
@@ -3,9 +3,11 @@
 import java.lang.*;
 import java.io.*;
 import java.util.*;
+import java.nio.*;
 
 public class FilePush {
     String pathname = "";
-    File f = new File(pathname);
+    File f = new File("car.txt");
+    
     
 }
