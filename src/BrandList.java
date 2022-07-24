ea3d983d122197dc9ecab0f8af61d905afbd3f23
ASUS <ASUS@LAPTOP-Q9LHHV8A>
2022-07-24 18:58:08 +0700

    Hoàn thành phần Load và Save file

--- src/BrandList.java
+++ src/BrandList.java
@@ -1,41 +1,91 @@
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
 
-public class BrandList extends ArrayList<Brand>{
+public class BrandList extends ArrayList<Brand> {
 
     public BrandList() {
         super();
     }
-    
-    public boolean loadFromFile(String l){
-        return false;
-        
-        
+
+    public boolean loadFromFile(String filename) {
+        try {
+            File f = new File(filename);
+            if (!f.exists()) {
+                System.err.println("Error: file doesnt exist");
+                return false;
+            } else {
+                System.out.println("Load from file success!");
+                
+                FileReader fd = new FileReader(f);
+                BufferedReader bf = new BufferedReader(fd);
+                String line;
+                while ((line = bf.readLine())!= null) {
+                    String[] ls = line.split(",|:");
+                    Brand br = new Brand();
+                    br.setBrandID(ls[0]);
+                    br.setBrandName(ls[1].substring(1));
+                    br.setSoundBrand(ls[2].substring(1));
+                    br.setPrice(Double.parseDouble(ls[3].substring(1)));
+                    this.add(br);
+                    //System.out.println(line);
+                }
+                fd.close();
+                return true;
+            }
+
+        } catch (Exception e) {
+            System.err.println("error: " + e.getMessage());
+            return false;
+        }
+
     }
-    
-    public boolean saveToFile(String s){
-        return false;
-        
+
+    public boolean saveToFile(String filename) {
+        try {
+            File f = new File(filename);
+            if (!f.exists()) {
+                System.err.println(f.getCanonicalFile() + " does not exists to write");
+                return false;
+            }
+            FileWriter fw = new FileWriter(f);
+            PrintWriter pw = new PrintWriter(fw);
+            for (int i = 0; i < this.size(); i++) {
+                Brand p = this.get(i);
+                pw.println(p);
+            }
+            pw.close();
+            fw.close();
+            System.out.println("succeed!");
+            return true;
+        } catch (Exception e) {
+            System.err.println("Error: cannot write data to file " + filename);
+            System.err.println("Reason: " + e.getMessage());
+            return false;
+        }
+
     }
-    
-    public void searchID(){
+
+    public void searchID() {
         if (this.isEmpty()) {
             System.out.println("Empty list. Cannot search any brand");
         } else {
             String sID = Inputter.inputString("Input brand id to search:");
             Brand br = this.search(sID);//search brand base on code
-            if(br == null)
+            if (br == null) {
                 System.out.println("Brand " + sID + " doesn't exist.");
-            else
+            } else {
                 System.out.println("Found: " + br);
+            }
         }
     }
-    
-    public Brand getUserChoice(){
+
+    public Brand getUserChoice() {
         return null;
-        
+
     }
-    
+
     public Brand search(String ID) {
         ID = ID.trim().toUpperCase();
         for (int i = 0; i < this.size(); i++) {
@@ -45,13 +95,13 @@ public class BrandList extends ArrayList<Brand>{
         }
         return null; // not found
     }
-    
+
     private boolean isCodeDupplicated(String code) {
         code = code.trim().toUpperCase();
         return search(code) != null;
     }
-    
-    public void addBrand() throws InterruptedException{
+
+    public void addBrand() throws InterruptedException {
         //input data of new brand
         String newID, newName, newSoundBrand = null;
         double newPrice;
@@ -68,23 +118,23 @@ public class BrandList extends ArrayList<Brand>{
         newName = newName.toUpperCase();
         newSoundBrand = Inputter.inputNonNlankStr("Sound brand of new brand: ");
         newSoundBrand = newSoundBrand.toUpperCase();
-        newPrice = Inputter.inputDouble(); 
+        newPrice = Inputter.inputDouble();
         // create new brand
         Brand br = new Brand(newID, newName, newSoundBrand, newPrice);
         //Add new brand to the list
         this.add(br);
         System.out.println("Brand " + newID + " has been added.");
     }
-    
-    public void updateBrand() throws InterruptedException{
-        if(this.isEmpty())
+
+    public void updateBrand() throws InterruptedException {
+        if (this.isEmpty()) {
             System.out.println("Empty list. No update can be performed!");
-        else{
+        } else {
             String uID = Inputter.inputString("Input ID of updated brand:");
             Brand br = this.search(uID);//search brand
-            if(br == null)
+            if (br == null) {
                 System.out.println("Brand " + uID + " doesn't existed!");
-            else{
+            } else {
                 //update brand id
                 String oldID = br.getBrandID();
                 String msg = "Old ID: " + oldID + ", new ID: ";
@@ -106,20 +156,21 @@ public class BrandList extends ArrayList<Brand>{
                 double newPrice = Inputter.inputDouble();
                 br.setPrice(newPrice);
                 System.out.println("Brand " + uID + " has been updated.");
-                
+
             }
         }
     }
-    
-    public void listBrands(){
-        if(this.isEmpty())
+
+    public void listBrands() {
+        if (this.isEmpty()) {
             System.out.println("Empty list!");
-        else{
+        } else {
             System.out.println("Brand list: ");
-            for(Brand br: this)
+            for (Brand br : this) {
                 System.out.println(br);
+            }
             System.out.println("Total: " + this.size() + " brand(s).");
         }
     }
-    
+
 }
