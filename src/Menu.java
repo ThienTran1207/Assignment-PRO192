acc5f30bbc5ba28b8d451ac0c3391683242752d6
NQMinh170486 <109325916+NQMinh170486@users.noreply.github.com>
2022-07-25 01:32:42 +0700

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -3,6 +3,24 @@ import java.lang.*;
 import java.util.*;
 
 public class Menu<E> {
+    public static int getChoice (String[] ops){
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < ops.length; i++) {
+            System.out.println("\n" + (i+1) + "-" + ops[i]);           
+        }
+        System.out.println("\n Choose 1..." + ops.length + ": ");
+        return Integer.parseInt(sc.nextLine());
+    }
+    
+      public E ref_getChoice(ArrayList<E> options){
+        int response;
+        int N = options.size();
+        do {            
+            response = int_getChoice(options); 
+        } while (response < 0 || response > N);
+        return options.get(response - 1);
+    }
+
     public int int_getChoice(ArrayList<E> options){
         int response;
         int N = options.size();
@@ -13,13 +31,5 @@ public class Menu<E> {
         }
         response = MyLibs.inputInt();
         return response;
-    }
-    public E ref_getChoice(ArrayList<E> options){
-        int response;
-        int N = options.size();
-        do {            
-            response = int_getChoice(options);
-        } while (response < 0 || response > N);
-        return options.get(response - 1);
     }
 }
