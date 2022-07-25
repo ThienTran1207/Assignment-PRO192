94887e65b3ca9fad61fe1cf7bd1f2ab4eaa83ab3
ZgsNat <88556390+ZgsNat@users.noreply.github.com>
2022-07-25 17:55:29 +0700

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -3,28 +3,10 @@ import java.lang.*;
 import java.util.*;
 
 public class Menu<E> {
-    public static int getChoice (String[] ops){
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < ops.length; i++) {
-            System.out.println("\n" + (i+1) + "-" + ops[i]);           
-        }
-        System.out.println("\n Choose 1..." + ops.length + ": ");
-        return Integer.parseInt(sc.nextLine());
-    }
-    
-      public E ref_getChoice(ArrayList<E> options){
-        int response;
-        int N = options.size();
-        do {            
-            response = int_getChoice(options); 
-        } while (response < 0 || response > N);
-        return options.get(response - 1);
-    }
-
     public int int_getChoice(ArrayList<E> options){
         int response;
         int N = options.size();
-        for (int i = 0; i <= N; i++){
+        for (int i = 0; i < N; i++){
             if(options.get(i)!=null){
                 System.out.println((i+1) + ": " + options.get(i));
             }
@@ -32,4 +14,13 @@ public class Menu<E> {
         response = MyLibs.inputInt();
         return response;
     }
+    public E ref_getChoice(ArrayList<E> options){
+        int response;
+        int N = options.size();
+        do {            
+            response = int_getChoice(options);
+        } while (response < 0 || response > N);
+        return options.get(response - 1);
+    }
 }
+
