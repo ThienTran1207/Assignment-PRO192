c47204607d57c01cb222766790eda83ac631f016
ZgsNat <88556390+ZgsNat@users.noreply.github.com>
2022-07-25 17:54:49 +0700

    Update CarManager.java

--- src/CarManager.java
+++ src/CarManager.java
@@ -21,19 +21,20 @@ public class CarManager {
         options.add("Update a car based on its ID");
         options.add("Save cars to file, named cars.txt");
         options.add("exit");
-        
+
         BrandList brandList = new BrandList();
+        brandList.loadFromFile("brands.txt");
         CarList carList = new CarList(brandList);
+        carList.loadFromFile("cars.txt");
+
         int Choice;
         Menu mn = new Menu();
         do {
             Choice = mn.int_getChoice(options);
             switch (Choice) {
                 case 1:
-                    if(brandList.loadFromFile("File_brands.txt")){
                     brandList.listBrands();
                     System.out.println("");
-                    }
                     break;
                 case 2:
                     brandList.addBrand();
@@ -53,39 +54,30 @@ public class CarManager {
                     System.out.println("");
                     break;
                 case 5:
-                    brandList.saveToFile("File_brands.txt");
+                    brandList.saveToFile("brands.txt");
                     System.out.println("");
                     break;
                 case 6:
-                    if(carList.loadFromFile("‪File_cars.txt")){
-                        carList.listCars();
-                        System.out.println("");
-                    }
+                    carList.listCars();
                     break;
                 case 7:
                     System.out.print("Enter the brand name: ");
                     carList.printBasedBrandName(MyLibs.inputstr());
                     break;
                 case 8:
-                    do{
                     carList.addCar();
-                    } while(!carList.saveToFile("File_cars.txt"));
                     System.out.println("");
                     break;
                 case 9:
-                    if(carList.loadFromFile("File_cars.txt") && carList.removeCar()){
-                        carList.saveToFile("File_cars.txt");
-                    }
+                    if(carList.removeCar())
                         System.out.println("");
                     break;
                 case 10:
-                    if(carList.loadFromFile("File_cars.txt") && carList.updateCar()){
-                        carList.saveToFile("File_cars.txt");
-                    }
+                    if(carList.updateCar())
                         System.out.println("");
                     break;
                 case 11:
-                    carList.saveToFile("File_cars.txt");
+                    carList.saveToFile("cars.txt");
                     break;
 
             }
